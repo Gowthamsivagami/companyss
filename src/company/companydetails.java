6def1d44b71127095def51a13b877c7400ecb110
gowthamgowtham50799@gmail.com <gowthamgowtham50799@gmail.com>
2024-12-28 15:50:35 +0530

    employee

---
+++ src/company/companydetails.java
@@ -0,0 +1,16 @@
+package company;
+
+public class companydetails {
+
+	private void name() {
+		System.out.println("company name : TCS");
+	}
+	private void Location() {
+		System.out.println("Company Location : Chennai");
+	}
+	public static void main(String[] args) {
+		companydetails c=new companydetails();
+		c.name();
+		c.Location();
+	}
+}
